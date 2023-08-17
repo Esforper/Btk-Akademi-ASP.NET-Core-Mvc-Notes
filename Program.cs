c4047bda83f237ca07b327afe39dde3d01e9e21c
Esforper <dolanbayomerfaruk@gmail.com>
2023-08-17 13:27:33 +0300

    9.2_Areas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;    //normalde sadece bu bir de Repositories in kalması gerekiyordu.
 using Microsoft.Extensions.Options;
@@ -11,23 +12,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews(); //servis kaydı yapıldı
 builder.Services.AddDbContext<RepositoryContext>(Options =>
 {
-    Options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"), b => b.MigrationsAssembly("StoreApp") );      //bu kayıt sayesinde ProductControllerda erişim yaptık
+    Options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"), b => b.MigrationsAssembly("StoreApp"));      //bu kayıt sayesinde ProductControllerda erişim yaptık
     //repositoryContext in newlenmesini sağlıyoruz, ihtiyacı olan bağlantı dizesini appsettings den alıp vermiş oluyor
     // sqlconnection = appsettings.json dosyasındaki şey
     //Btk Akademi asp.net core mvc 4.6 . video
 });
 // ne zaman RepositoryContext gerekirse bir bağlantı oluşacak
 
-builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 //ICategoryRepository ifadesi için CategoryRepository yi tanımlamış olduk
 
 //IRepositoryManager arabirimi ile karşılaşırsa RepositoryManager classını getirecek
 
-builder.Services.AddScoped<IServiceManager,ServiceManager>();
-builder.Services.AddScoped<IProductService ,ProductManager>();
-builder.Services.AddScoped<ICategoryService,CategoryManager>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 //Configuration işlemi
 
 
@@ -38,12 +39,24 @@ app.UseStaticFiles();   //uygulamanın statik dosyalar kullanabileceğini belirt
 //bootstrap , jquery , font-awsome gibi kütüphaneleri indirmemize de yarıyor
 //bunları indirirken libman dan yararlanarak indiriyoruz
 
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapAreaControllerRoute(
+        name:"Admin",
+        areaName:"Admin",
+        pattern:"Admin/{controller=Dashboard}/{action=Index}/{id?}"
+    );
+    endpoint.MapControllerRoute(name:"default",pattern:"{controller=Home}/{action=Index}/{id?}");
+});
 
 app.UseRouting(); //uygulamanın viewse , controllera ihtiyacının yanı sıra bir routinge ihtiyacı var
 app.UseHttpsRedirection();  //Redirectionlar da işletilebilir (ekleyebiliriz) ?
-app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}"); //tanım yaptık
- /* app.MapControllerRoute(
-    name:"default",
-    pattern:"{controller=Home}/{action=Index}/{id?}"); şeklinde de tanımlanabilir */
+//app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}"); //tanım yaptık
+//9.2 de artık bunu kullanmıyoruz
+/* app.MapControllerRoute(
+   name:"default",
+   pattern:"{controller=Home}/{action=Index}/{id?}"); şeklinde de tanımlanabilir */
+// * burada tek bir end point tanımı var sadece
+
 
 app.Run();
